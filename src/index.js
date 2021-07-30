f36752514a4ee1a5252bc148fdfe15430dd70a6b
Andre Borges <a-filipe@hotmail.fr>
2021-07-30 16:22:37 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './index.css';
+import Router from "./components/Router";
+import reportWebVitals from './reportWebVitals';
+
+render(<Router />, document.querySelector('#root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
