f8a33e06f61443d96a781cf98c3f4eed86e98feb
Andre <a-filipe@hotmail.fr>
2021-09-22 09:21:56 +0200

    Changed antd and js with chakra and ts

---
+++ src/components/Home.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  Box,
+  Grid,
+} from "@chakra-ui/react"
+import NavBar from "./NavBar";
+import Product from "./Product";
+import Footer from "./Footer";
+
+function Home() {
+
+  return (
+    <>
+      <NavBar />
+
+      <Box p={4}>
+      <Grid templateColumns="repeat(4, 1fr)" gap={6}>
+        { [...Array(4)].map((e, i) => 
+                <Box w="100%" h="10" > 
+                <Product />
+              </Box>
+        )}
+      </Grid>
+      </Box>
+
+      {/* <Footer /> */}
+    </>
+  );
+}
+export default Home;
