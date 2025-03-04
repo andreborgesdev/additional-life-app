f17700c7b1c302cf5499bc33975ee7966c09edf9
André Borges <a-filipe@hotmail.fr>
2025-03-04 23:59:43 +0100

    Configure ESLint and TypeScript to ignore errors during production builds

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const { i18n } = require("./next-i18next.config");
 const nextConfig = {
   i18n,
   reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
